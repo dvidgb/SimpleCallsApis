c2b060b08319f53e37081993248fdaaf3f746aee
David Bueno <davidbuenodbc@gmail.com>
2023-05-01 12:03:39 +0200

    wip marvel characters list
    create project

---
+++ MarvelCharactersList/MarvelCharactersList/MarvelCharactersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharactersListApp.swift
+//  MarvelCharactersList
+//
+//  Created by David Bueno Castro on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharactersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
