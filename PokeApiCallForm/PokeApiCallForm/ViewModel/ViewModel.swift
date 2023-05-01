0c2f3b65cded187d895067aeb3905a2523185bcd
David Bueno <davidbuenodbc@gmail.com>
2023-05-01 13:38:17 +0200

    wip PokeApi
    create class ViewModel

---
+++ PokeApiCallForm/PokeApiCallForm/ViewModel/ViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ViewModel.swift
+//  PokeApiCallForm
+//
+//  Created by David Bueno Castro on 1/5/23.
+//
+
+import Foundation
+
+struct PokemonDataModel: Decodable {
+    let name: String
+    let url: String
+}
+
+struct PokemonResponseDataModel: Decodable {
+    let pokemons: [PokemonDataModel]
+    
+    enum CodingKeys: String, CodingKey {
+        case results
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.pokemons = try container.decode([PokemonDataModel].self, forKey: .results)
+    }
+}
